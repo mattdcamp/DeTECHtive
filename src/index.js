9e8bc20801271bed8b4ac74a09f0b40af9728bdd
Matt Campbell <matt.dcamp@gmail.com>
2016-07-31 18:28:53 -0700

    Create Test Data & Implement File Reader
    Program needs to be able to read test data from the filesystem. Adding that ability.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+"use strict";
 
+var parameter = require('./parameter.js'),
+    eventFactory = require('./eventFactory.js');
+
+console.log(process.argv);
+
+fs.readFile(process.argv, 'utf8', function(err, data) {
+    var parsedParameters, eventTreeRoots, output=[], i;
+
+    if(err) {
+        console.error("Could not open file: %s", err);
+        return;
+    }
+
+    parsedParameters = parameter.parseParameters(data);
+    for(i=0; i<parsedParameters.length; i++) {
+        eventFactory.createTimeline(parsedParameters[i]);
+    }
+
+    eventTreeRoots = eventFactory.getRoots();
+    for(i=0; i<eventTreeRoots.length; i++) {
+        output.push(eventTreeRoots[i].traverse());
+    }
+    console.log(JSON.stringify(output));
+});
